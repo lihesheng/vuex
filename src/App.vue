1e6ba7661fb48e4d95817913bc89dd9ec5239e1d
unknown <425061140@qq.com>
2020-11-14 17:14:11 +0800

    最后提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,102 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <a-input placeholder="请输入任务" @change="getValue" :value="inputDefault" class="my_ipt" />
+    <a-button type="primary" @click="addEvent">添加事项</a-button>
+
+    <a-list bordered :dataSource="infoList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <!-- :checked是复选框选中 -->
+        <a-checkbox :checked='item.done' @change="(e) => {changeStatus(e,item.id)}">{{ item.info }}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="removeEvent(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{unFailItem}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="stringKey === 'all' ? 'primary' : 'default'" @click="All('all')">全部</a-button>
+          <a-button :type="stringKey === 'undone' ? 'primary' : 'default'" @click="All('undone')">未完成</a-button>
+          <a-button :type="stringKey === 'done' ? 'primary' : 'default'" @click="All('done')">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="deleComplate">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
+<script>
+import {mapState , mapGetters} from 'vuex'
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  //请求数据
+  created(){
+    this.$store.dispatch('getList')
+  },
+  //挂载属性
+  computed:{
+    /* 在最后一步要将这里来替换 list 换成 infoList */
+    // ...mapState(['list','inputDefault','count','stringKey']),
+    ...mapState(['inputDefault','count','stringKey']),
+    ...mapGetters(['unFailItem','infoList'])
+  },
+  methods:{
+    //获取文本框种的值
+    getValue(e){
+      //console.log(e.target.value)
+      this.$store.commit('changeInputData',e.target.value)
+    },
+    //数据的添加
+    addEvent(){
+      this.$store.commit('addItem')
+    },
+    //删除数据
+    removeEvent(id){
+      this.$store.commit('removeItem',id)
+    },
+    //改变复选框状态
+    changeStatus(e,id){
+      //console.log(e.target.checked)
+      const params = {
+        id:id,
+        done:e.target.checked
+      }
+      this.$store.commit('changeItemDone',params)
+    },
+    //清除已完成
+    deleComplate(){
+      this.$store.commit('deleteComplate')
+    },
+    //切换按钮的高亮
+    All(key){
+      this.$store.commit('Allchange',key)
+      // console.log(key)
+    }
+  }
+};
+</script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  padding: 10px;
 }
-
-#nav {
-  padding: 30px;
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,102 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    //原数组
+    list:[],
+    //为了实现输入文本双向绑定
+    inputDefault:'请输入要添加的文本',
+    //暂时定义的一个数据，因为原数据id最大是4
+    nextId:5,
+    //为了切换按钮的高亮定义
+    stringKey:'all'
   },
   mutations: {
+    //修改数据，并进行赋值
+    initlist(state,list){
+      state.list = list
+    },
+
+    //实时更新inputDafault的数据
+    changeInputData(state,value){
+      state.inputDefault = value
+    },
+
+    //实现数据的添加
+    addItem(state){
+      let obj = {
+        //这里的id需要格外的注意
+        id:state.nextId,
+        info:state.inputDefault,
+        done:false
+      }
+      state.list.push(obj)
+      state.nextId ++
+      state.inputDefault = ' '
+      //console.log(state.nextId)
+    },
+
+    removeItem(state,id){
+      let index = state.list.findIndex(item => item.id === id)  
+      if(index == -1){
+        throw new error('没有找到对应的索引')
+      }
+      state.list.splice(index,1)
+      //console.log(index)
+    },
+
+    //修改state种的done属性
+    changeItemDone(state,params){
+      //根据id找出对应的索引
+      const i = state.list.findIndex(item => item.id === params.id)
+      //根据索引进行改值
+      state.list[i].done = params.done
+    },
+
+    //清除已完成的事件
+    deleteComplate(state){
+      //这里还是折腾了好久，自己写的还是不如老师写的
+      state.list = state.list.filter(item => item.done == false)
+    },
+
+    //点击实现高亮切换
+    Allchange(state,key){
+     state.stringKey = key
+    },
+    
   },
   actions: {
+    //axios是异步操作
+    getList(context){
+      axios.get('./list.json').then(({data}) => {
+        //console.log(data)
+       context.commit('initlist',data)
+      })
+     }
   },
   modules: {
-  }
+
+  },
+  //计算还有多少条剩余
+  getters:{
+    unFailItem(state){
+      return state.list.filter(item => item.done == false).length
+    },
+    //整个项目最后的操作
+    infoList(state){
+      if(state.stringKey === 'all'){
+        return state.list
+      }else if (state.stringKey === 'undone'){
+        return state.list.filter(item => !item.done)
+      }else if (state.stringKey === 'done') {
+        return state.list.filter(item => item.done)
+      }else {
+        return state.list
+      }
+    }
+  },
 })
